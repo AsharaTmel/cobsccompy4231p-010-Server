873a3745ab2f2c8d7f0766afa19735fc296710de
AsharaTmel <asharatme@gmail..com>
2024-07-19 18:48:21 +0530

    checking if cloning works, since I have deleted my local folder by a mistake

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, this is your basic Node.js server!');
+    res.end('Hey, this is your basic Node.js server!');
 });
 
 const PORT = 3000;
